51b9fecf344e267340f32a511047cb94aaf41b3c
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2024-09-01 19:12:52 +0100

    add: new stats elements

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
+import Stack from '@mui/material/Stack';
+import { Gauge } from '@mui/x-charts/Gauge';
 import axios from "axios";
 
 interface JobData {
@@ -25,7 +27,7 @@ export default function Statistics()  {
     useEffect(() => {
        getData();
 
-    });
+    }, [jobData]);
     const jobMap = new Map<string, number>();
 
     if (jobData.length !== 0) {
@@ -45,17 +47,19 @@ export default function Statistics()  {
         value: count,
         label: jobType,
     }));
-    const top5JobTypes = jobDataFormatted
+    const topJobTypes = jobDataFormatted
         .sort((a, b) => b.value - a.value)
         .slice(0, 5);
 
 
   return (
+      <>
+      <div className="grid grid-cols-1 ">
       <div className="grid grid-cols-2 gap-1 mt-10">
           <PieChart
               series={[
                   {
-                      data: top5JobTypes,
+                      data: topJobTypes,
                       highlightScope: { faded: 'global', highlighted: 'item' },
                       faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
                   },
@@ -64,6 +68,28 @@ export default function Statistics()  {
               height={300}
           />
       </div>
-
+         </div>
+          <span className="flex items-center mt-5">
+    <span className="h-px flex-1 bg-black"></span>
+    <span className="shrink-0 px-6">top 5 job offers posts numbers</span>
+    <span className="h-px flex-1 bg-black"></span>
+          </span>
+    <div className="grid grid-cols-1 ">
+      <div className='grid grid-cols-5 gap-1 mt-10 ml-10'>
+          <Stack
+              direction={{ xs: 'column', md: 'row' }}
+              spacing={{ xs: 1, md:5 }}
+          >
+              <h3 className="min-w-32">Number of Jobs:</h3>
+              {topJobTypes.map((job, index) => (
+                  <div key={index}>
+                      <h2 className="items-center text-center">{job.label}</h2>
+                      <Gauge width={200} height={80} value={job.value} valueMax={jobData.length} />
+                  </div>
+              ))}
+          </Stack>
+      </div>
+      </div>
+</>
   );
 };
